f3642a989b4a4715cce82d97a246d89de18eea22
黄敏争 <ahtchmz@163.com>
2015-10-15 14:22:21 +0800

    测试.gitignore

--- imooc_zhy/src/com/zhy/MainActivity.java
+++ imooc_zhy/src/com/zhy/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends FragmentActivity implements OnClickListener,
 		Log.i("main", "将项目导入到android studio ");
 		Log.i("main","在android studio 里做修改 ");
 		Log.i("main","在android studio 里再做修改，主要是为了忽略掉一些提交的文件 ");
-		Log.i("main","在android studio 里再做修改，主要是为了忽略文件imooc_zhy/.idea/workspace.xml，这是自动生成的。不需要提交");
+		Log.i("main","在android studio 里再做修改，主要是为了忽略文件/imooc_zhy/.idea/workspace.xml，这是自动生成的。不需要提交");
 	}
 
 	/**
