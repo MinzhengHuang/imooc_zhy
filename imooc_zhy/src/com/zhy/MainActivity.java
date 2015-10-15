762fa8e445949444a7354ff93af79d371981ec1f
黄敏争 <ahtchmz@163.com>
2015-10-15 11:39:02 +0800

    添加.gitignore文件

--- imooc_zhy/src/com/zhy/MainActivity.java
+++ imooc_zhy/src/com/zhy/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends FragmentActivity implements OnClickListener,
 		Log.i("main", "将项目导入到android studio ");
 		Log.i("main","在android studio 里做修改 ");
 		Log.i("main","在android studio 里再做修改，主要是为了忽略掉一些提交的文件 ");
+		Log.i("main","在android studio 里再做修改，主要是为了忽略文件夹bin和文件.idea");
 	}
 
 	/**
