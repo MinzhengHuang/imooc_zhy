2bd779c56ff849e1ffde2e8a9b1b0ae9d0547cae
黄敏争 <ahtchmz@163.com>
2015-10-30 10:39:59 +0800

    添加ImagIVeeAndroid 高清加载巨图方案 拒绝压缩图片

---
+++ imooc_zhy/src/com/zhy/LargeImageViewActivity.java
@@ -0,0 +1,50 @@
+package com.zhy;
+
+/**
+ * Created by huangminzheng on 15/10/28.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zhy.largeimage.view.LargeImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class LargeImageViewActivity extends Activity {
+    private LargeImageView mLargeImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_large_image_view);
+
+        mLargeImageView = (LargeImageView) findViewById(R.id.id_largetImageview);
+        try {
+            InputStream inputStream = getAssets().open("qm.jpg");
+            mLargeImageView.setInputStream(inputStream);
+
+            //            //获得图片的宽、高
+//            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+//            tmpOptions.inJustDecodeBounds = true;
+//            BitmapFactory.decodeStream(inputStream, null, tmpOptions);
+//            int width = tmpOptions.outWidth;
+//            int height = tmpOptions.outHeight;
+//
+//            //设置显示图片的中心区域
+//            BitmapRegionDecoder bitmapRegionDecoder = BitmapRegionDecoder.newInstance(inputStream, false);
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inPreferredConfig = Bitmap.Config.RGB_565;
+//            Bitmap bitmap = bitmapRegionDecoder.decodeRegion(new Rect(width / 2 - 100, height / 2 - 100, width / 2 + 100, height / 2 + 100), options);
+//            mImageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
+
---
+++ imooc_zhy/src/com/zhy/flowlayout/CategoryActivity.java
@@ -0,0 +1,67 @@
+package com.zhy.flowlayout;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.zhy.R;
+
+
+public class CategoryActivity extends FragmentActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    private String[] mTabTitles = new String[]
+            {"Muli Selected", "Limit 3",
+                    "Event Test", "ScrollView Test", "Single Choose"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        mTabLayout = (TabLayout) findViewById(R.id.id_tablayout);
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                switch (i) {
+                    case 0:
+                        return new SimpleFragment();
+                    case 1:
+                        return new LimitSelectedFragment();
+                    case 2:
+                        return new EventTestFragment();
+                    case 3:
+                        return new ScrollViewTestFragment();
+                    case 4:
+                        return new SingleChooseFragment();
+                    default:
+                        return new EventTestFragment();
+                }
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+
+                return mTabTitles[position];
+            }
+
+            @Override
+            public int getCount() {
+                return mTabTitles.length;
+            }
+        });
+
+
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+}
