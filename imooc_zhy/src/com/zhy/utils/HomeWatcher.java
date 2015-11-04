6c504f3d558aec859fbab0bceb8328b4fbed18ea
黄敏争 <ahtchmz@163.com>
2015-11-04 15:49:21 +0800

    format

--- imooc_zhy/src/com/zhy/utils/HomeWatcher.java
+++ imooc_zhy/src/com/zhy/utils/HomeWatcher.java
@@ -74,7 +74,7 @@ public class HomeWatcher {
 			if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
 				String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
 				if (reason != null) {
-					L.i(TAG, "action:" + action + ",reason:" + reason);
+					LogUtils.i(TAG, "action:" + action + ",reason:" + reason);
 					if (mListener != null) {
 						if (reason.equals(SYSTEM_DIALOG_REASON_HOME_KEY)) {
 							// 短按home键
